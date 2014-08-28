510d6818f635ff45d8797a76d9124e11df6252ef
Victor Queiroz <victorcqueirozg@gmail.com>
2014-08-27 22:23:27 -0300

    full test implemented

--- test/components/ngCurrencyMask-Directive-Spec.js
+++ test/components/ngCurrencyMask-Directive-Spec.js
@@ -17,24 +17,50 @@
 			beforeEach(module(dependency));
 		});
 
-		describe('Directives', function () {
-			var $rootScope, $compile, scope, element;
+		describe('directives', function () {
+			var $rootScope, $compile, scope, element, form;
 
 			beforeEach(inject(function ($injector) {
 				$rootScope = $injector.get('$rootScope');
 				$compile = $injector.get('$compile');
 				scope = $rootScope.$new();
-				scope.currency = 1000000;
+				scope.currency = 100000;
 			}));
 
 			describe('ngCurrencyMask directive', function () {
 				beforeEach(function () {
-					element = '<form>' +
-						'<input type="text" ng-model="currency" ng-currency-mask>' +
-						'<input type="text" ng-model="currency" class="clean-of-mask">'
-					'</form>';
+					element = angular.element('<form name="form">' +
+						'<input type="text" name="masked" ng-model="currency" ng-currency-mask>' +
+						'<input type="text" name="unmasked" ng-model="currency" class="clean-of-mask">' +
+					'</form>');
 					element = $compile(element)(scope);
+					form = scope.form;
 					scope.$digest();
+				});
+
+				it('should have masked content', function () {
+					expect(form.masked.$viewValue).toBe('R$ 1.000,00');
+				});
+
+				it('should have unmasked content', function () {
+					var $viewValue = Number(form.unmasked.$viewValue);
+					expect($viewValue).toBe(100000);
+				});
+
+				it('should change the masked input', function () {
+					scope.currency = 1020;
+					scope.$digest();
+
+					expect(form.masked.$viewValue).toBe('R$ 10,20');
+				});
+
+				it('should change the unmasked input', function () {
+					scope.currency = 1020;
+					scope.$digest();
+
+					var $viewValue = Number(form.unmasked.$viewValue);
+
+					expect($viewValue).toBe(1020);
 				});
 			});
 		});
