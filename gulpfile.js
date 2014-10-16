f3992391eaad8c75400a89c9d1c1d765f6c4eeb0
Victor Queiroz <victorcqueirozg@gmail.com>
2014-10-16 12:57:17 -0300

    chore(dist/ng-currency-mask.min.js): minified dist bug fixed

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var path = require('path');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var sourcemaps = require('gulp-sourcemaps');
+var ngAnnotate = require('gulp-ng-annotate');
 var del = require('del');
 var karma = require('karma').server;
 
@@ -25,12 +26,14 @@ gulp.task('test', function (done) {
 gulp.task('build', ['clean'], function () {
 	gulp.src(paths.scripts)
 		.pipe(sourcemaps.init())
+			.pipe(ngAnnotate())
 			.pipe(concat('ng-currency-mask.js'))
 		.pipe(sourcemaps.write())
 		.pipe(gulp.dest('dist'));
 
 	return gulp.src(paths.scripts)
 		.pipe(sourcemaps.init())
+			.pipe(ngAnnotate())
 			.pipe(uglify())
 			.pipe(concat('ng-currency-mask.min.js'))
 		.pipe(sourcemaps.write())
