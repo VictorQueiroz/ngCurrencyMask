32a2a5da6c9190fe9911ecbc95071130c1fc1ee1
Victor Queiroz <victorcqueirozg@gmail.com>
2015-07-20 20:09:21 -0300

    Consuming `$locale` data while replacing the currency.
    Whole code refactored and files more organized in just one folder, for it's just unecessary separate tiny services in multiple files.
    
    Stop adding the unconvenient white space after the currency.
    
    Signed-off-by: Victor Queiroz <victorcqueirozg@gmail.com>

--- src/components/providers.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular
-	.module('ngCurrencyMask.providers', [
-		'ngCurrencyMask.providers.masker'
-	]);
--- test/module.spec.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-describe('ngCurrencyMask', function () {
-	var MODULE_NAME = 'ngCurrencyMask';
-
-	beforeEach(module('ngCurrencyMask'));
-});
